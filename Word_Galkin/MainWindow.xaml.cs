1a9d68e6475dca65deee7fada41202d56a3b8394
Nikita <galkinnikita228@gmail.com>
2024-06-03 15:19:25 +0500

    Выполненная практическая работа на оценку удовлетворительно. Galkin

--- Word_Galkin/MainWindow.xaml.cs
+++ Word_Galkin/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Word_Galkin.Context;
 
 namespace Word_Galkin
 {
@@ -23,6 +25,23 @@ namespace Word_Galkin
         public MainWindow()
         {
             InitializeComponent();
+            LoadRooms();
+        }
+
+        private void Report(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Word Files (*docx)|*.docx";
+            sfd.ShowDialog();
+            if (sfd.FileName != "")
+            {
+                OwnerContext.Report(sfd.FileName);
+            }
+        }
+
+        public void LoadRooms()
+        {
+            for (int i = 1; i < 20; i++) Parent.Children.Add(new Elements.Room(i));
         }
     }
 }
